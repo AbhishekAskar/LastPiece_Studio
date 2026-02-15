92bdb4b147179543a849324a3ab89ec2a35b4f53
Abhishek <abhiaskar.18@gmail.com>
2026-02-15 14:25:05 +0530

    Remove Astro migration

--- astro-app/src/lib/animations.ts
+++
@@ -1,5 +0,0 @@
-// Animations have been replaced with CSS classes
-export const VARIANTS_HERO = {};
-export const VARIANTS_CONTAINER = {};
-export const VARIANTS_SECTION = {};
-export const VARIANTS_CARD = {};
